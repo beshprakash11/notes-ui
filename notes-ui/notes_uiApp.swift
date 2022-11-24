35a1557d72d261aca54375b1623355239624364c
Besh P.Yogi <beshprakash@Beshs-Mac.local>
2022-11-24 09:37:48 +0100

    First Project

---
+++ notes-ui/notes_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_uiApp.swift
+//  notes-ui
+//
+//  Created by Besh P.Yogi on 24.11.22.
+//
+
+import SwiftUI
+
+@main
+struct notes_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
